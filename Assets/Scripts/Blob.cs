e65c041fcb2d71867d4374c7710e474c76b3b5db
sam <skb502@nyu.edu>
2021-02-24 23:17:10 -0500

    fixed being able to enter shrinking state multiple times

--- Assets/Scripts/Blob.cs
+++ Assets/Scripts/Blob.cs
@@ -33,7 +33,8 @@ public class Blob : MonoBehaviour
     // Change blobs to shrinking state when clicked.
     void OnMouseDown()
     {
-        ChangeState(new BlobStateShrinking(this)); 
+        if (!currentState is BlobStateShrinking)
+            ChangeState(new BlobStateShrinking(this)); 
     }
 
     // Destroy blob gameObject and remove it from master blob list.
