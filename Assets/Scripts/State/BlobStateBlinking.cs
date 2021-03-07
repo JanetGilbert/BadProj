1206401e59f362b177edf05b4779ecd76b626eb3
Charlee <liucharlee@gmail.com>
2021-03-06 21:18:49 -0800

    tried to make blobs blink

---
+++ Assets/Scripts/State/BlobStateBlinking.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobStateBlinking : BlobState
+{
+    //public Renderer rend;
+
+    private float elapsedTime = 0f;
+    private float endTime = 0.5f;
+   
+    public BlobStateBlinking(Blob theBlob) : base(theBlob) // Derived class constructor calls base class constructor.
+    {
+
+    }
+
+    public override void Run() // Overriden from base class.
+    {
+        /*elapsedTime += Time.deltaTime;
+
+        if (elapsedTime > endTime)
+        {
+            renderer.enabled = false;
+        }*/
+
+        /*if (elapsedTime > endTime + 1f/Time.deltaTime){
+                renderer.enabled = true;
+
+                elapsedTime = 0;
+
+        }*/
+              
+
+    }
+
+    public override void Enter() // Overriden from base class.
+    {
+        base.Enter(); // Call base class.
+
+        //rend = GetComponent<Renderer>();
+
+    }
+}
